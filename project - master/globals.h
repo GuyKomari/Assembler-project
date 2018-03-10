3f25094d459e7012aa138999abdec1e3929a4daa
Gil <gil619gil@hotmail.com>
2018-03-10 03:23:01 +0200

    Not Necessary

--- project - master/globals.h
+++
@@ -1,6 +0,0 @@
-typedef enum {false, true} bool;
-
-
-/*
-Here is all the global variables, registers (r0, .... , r7), definitions, commands("mov", "cmp", ... . "lea" etc.), structures, ....
-*/
--- project/output.h
+++
@@ -1,3 +0,0 @@
-/*
-Handles the output procedures
-*/
