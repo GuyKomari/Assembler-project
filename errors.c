0cfef1ae2a6595f0af743b9f980f59ed7a1f789a
GuyKomari <35179749+GuyKomari@users.noreply.github.com>
2018-03-15 02:54:18 +0200

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,8 +1,14 @@
 #include "errors.h"
 
+void printFileError(char* error, char* fileName)
+{
+	fprintf(stderr, "%s %s\n", "[Error] -> ", error, fileName);
+}
+
+
 void printError(char* error)
 {
-	fprintf(stderr, "%s %s\n", "[Error] -> ", error);
+	fprintf(stderr, "[IN LINE - %d] %s %s\n", lineCounter, "[Error] -> ", error);
 }
 
 void printWarning(char* warn)
