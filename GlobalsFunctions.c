242a765ca6c075e5d33361081a49c32b6ceee85b
GuyKomari <35179749+GuyKomari@users.noreply.github.com>
2018-03-25 16:29:51 +0300

    Update GlobalsFunctions.c

--- GlobalsFunctions.c
+++ GlobalsFunctions.c
@@ -1,7 +1,5 @@
 #include "GlobalsFunctions.h"
 
-
-
 bool isRegister(char *token)
 {
 	int i;
@@ -28,63 +26,75 @@ bool isOpcode(char *token)
 
 bool isLabel(char* src, char* dest)
 {
-	bool isExternDef, isEntryDef;
+	bool res, flag;
 	char *temp, *start;
 	int len;
+	res = TRUE;
+	flag = FALSE;
 	temp = trimStr(src);
-	len = 0;
-	isExternDef = isEntryDef = FALSE;
-	if (isExtern(temp))
-	{
-		isExternDef = TRUE;
-		temp = (char *)(temp + EXTERN_LENGTH);
-	}
-	else if (isEntry(temp))
-	{
-		isEntryDef = TRUE;
-		temp = (char *)(temp + ENTRY_LENGTH);
-	}
-	temp = trimLeftStr(temp);
 	start = temp;
+	len = 0;
+	if (strstr(temp, EXTERN_DECLERATION))
+	{
+		temp = strstr(temp, EXTERN_DECLERATION);
+		temp = (char *)(temp + EXTERN_LENGTH);
+		temp = trimLeftStr(temp);
+		if (checkLabel(src, EXTERN))
+		{
+			if(dest!='\0')
+				strcpy(dest, temp);
+			return TRUE;
+		}
+		return FALSE;
+	}
+	else if (strstr(temp, ENTRY_DECLERATION))
+	{
+		temp = strstr(temp, ENTRY_DECLERATION);
+		temp = (char *)(temp + ENTRY_LENGTH);
+		temp = trimLeftStr(temp);
+		if (checkLabel(src, ENTRY))
+		{
+			if (dest != '\0')
+				strcpy(dest, temp);
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	if (temp[0] == '.')/*case - data : .data/.string ...*/
+		return FALSE;
+	/*else - sentence begins with a label*/
 	if (!(isalpha(*temp)))/*label doesnt start with a letter*/
 	{
-		/*printError("The label does not begins with an alpha letter");*/
-		return FALSE;
+		printError(LABEL_NOT_START_WITH_ALPHA);
+		res = FALSE;
 	}
 	while (*temp && !isspace(*temp) && strncmp((char*)(temp), ":", 1) != 0)
 	{
-		if (*temp && !isalpha(*temp) && !isdigit(*temp))
+		if (!flag && *temp && !isalpha(*temp) && !isdigit(*temp))
 		{
-			/*printError("The label contains non-alpha or non-digit characters");*/
-			return FALSE;
+			printError(LABEL_CONTAINS_NON_ALPHA_OR_DIGIT);
+			res = FALSE;
+			flag = TRUE;
 		}
 		temp++;
 		len++;
 	}
-	if (!isExternDef && strncmp((char*)(temp++), ":", 1) != 0)
+	if (strncmp((char*)(temp++), ":", 1) != 0)
 	{
-		/*printError("A colon is missing");*/
 		return FALSE;
 	}
-	if (!isExternDef && !isspace(*temp))
+	if (len > MAX_LABEL_SIZE)
 	{
-		if (ispunct(*temp) || isdigit(*temp) || isalpha(*temp))
-		{
-			/*printError("additional character after declaration of a label");*/
-			return FALSE;
-		}
-		/*printError("missing a space");*/
+		printError(LABEL_LENGTH_ERROR);
 		return FALSE;
 	}
 	if (dest != NULL)
 		strncpy(dest, start, len);
-	return TRUE;
+	return res;
 }
-/*
-headOfSymbolsList - head of the symbols list;
-token - is a valid label
-lineCounter - the code line
-*/
+
+
 bool isLabelDefined(symbolPtr *headOfSymbolsList, char* token)
 {
 	symbolPtr temp = *headOfSymbolsList;
@@ -104,62 +114,18 @@ bool isLabelDefined(symbolPtr *headOfSymbolsList, char* token)
 bool isEntry(char* line)
 {
 	char *temp = trimStr(line);
-	if (strncmp(temp, ".entry", ENTRY_LENGTH))
-		return FALSE;
-	temp = (char *)(temp + ENTRY_LENGTH);
-
-	if (!isspace(*temp))
-		return FALSE;
-
-	while (isspace(*temp))
-		temp++;
-
-	if (!(isalpha(*temp)))
-		return FALSE;
-
-	while (*temp)
-	{
-		if (isspace(*temp))
-			return FALSE;
-		temp++;
-	}
-	return TRUE;
+	if (strstr(temp, ENTRY_DECLERATION))
+		return TRUE;
+	return FALSE;
 }
 
 
 bool isExtern(char* line)
 {
-	int i;
 	char *temp = trimStr(line);
-	if (strncmp(temp, ".extern", EXTERN_LENGTH))
-		return FALSE;
-	temp = (char *)(temp + EXTERN_LENGTH);
-
-	if (!isspace(*temp))
-		return FALSE;
-
-	while (isspace(*temp))
-		temp++;
-
-	if (!(isalpha(*temp)))
-		return FALSE;
-
-	for (i = 0; i < NUM_OF_KEYWORDS; i++)
-	{
-		if (strcmp(temp, Keywords[i]) == 0)
-		{
-			printError(VARIABLE_NAME_EQUALS_KEYWORD_NAME);
-			return FALSE;
-		}
-	}
-
-	while (*temp)
-	{
-		if (isspace(*temp))
-			return FALSE;
-		temp++;
-	}
-	return TRUE;
+	if (strstr(temp, EXTERN_DECLERATION))
+		return TRUE;
+	return FALSE;
 }
 
 
@@ -177,7 +143,7 @@ bool isValidLabel(char* token)
 		return FALSE;
 	for (; i < length; i++)
 	{
-		if (trimmed[i] < 0 || trimmed[i] > 255)
+		if (trimmed[i] < 0 || trimmed[i] > 255 || trimmed[i] == '.')
 			return FALSE;
 		if (!(isalpha(trimmed[i]) || isdigit(trimmed[i])))
 			return FALSE;
@@ -185,21 +151,88 @@ bool isValidLabel(char* token)
 	return TRUE;
 }
 
+
+
+bool checkLabel(char *line , char *type)
+{
+	int i;
+	char* temp;
+	bool res = TRUE;
+	if (strcmp(type, EXTERN) == 0 || strcmp(type, ENTRY) == 0)
+	{
+		if (strcmp(type, EXTERN) == 0)
+		{
+			temp = strstr(line,EXTERN_DECLERATION);
+			temp += EXTERN_LENGTH;
+			temp = trimStr(temp);
+		}
+		else/*entry*/
+		{
+			temp = strstr(line, ENTRY_DECLERATION);
+			temp += ENTRY_LENGTH;
+			temp = trimStr(temp);
+		}
+	}
+	temp = trimStr(temp);
+	if (strlen(temp) == 0)
+	{
+		printError(NO_LABEL_DECLARED_ERROR);
+		return FALSE;
+	}
+	if (strlen(temp) > MAX_LABEL_SIZE)
+	{
+		printError(LABEL_LENGTH_ERROR);
+		return FALSE;
+	}
+	if (!isalpha(temp[0]))
+	{
+		printError(LABEL_NOT_START_WITH_ALPHA);
+		res = FALSE;
+	}
+	for (i = 1; i < strlen(temp); i++)
+	{
+		if (!(isalpha(temp[i]) || isdigit(temp[i])))
+		{
+			if (isspace(temp[i]))
+			{
+				printError(MORE_THEN_ONE_LABEL);
+			}
+			else
+			{
+				printError(LABEL_CONTAINS_NON_ALPHA_OR_DIGIT);
+			}
+			return FALSE;
+		}
+	}
+	return res;
+}
+
 bool isNumOperand(char* token)
 {
-	int  i = 0;
+	int  i = 0, num;
 	char* trimmed;
 	trimmed = trimStr(token);
 	if (trimmed[i++] != '#')
 		return FALSE;
-	if (atoi(trimmed + i) == 0)
+	num = atoi(trimmed + i);
+	if (num == 0)
 	{
 		if (trimmed[i] == '+' || trimmed[i] == '-')
 			i++;
 		if (strcmp(trimmed + 1, "0") != 0)
 			return FALSE;
 	}
-	return TRUE;
+	trimmed++;
+	if (num > MAX_NUMBER || num < MIN_NUMBER)
+	{
+		if(num > MAX_NUMBER)
+			printExtendedError(OVERFLOW_COMMAND_POSITIVE_NUMBER, trimmed);
+		else 
+			printExtendedError(OVERFLOW_COMMAND_NEGATIVE_NUMBER, trimmed);
+		return FALSE;
+	}
+	else
+		return TRUE;
 }
 
 
@@ -272,11 +305,11 @@ bool isFileExists(char* fileName)
 }
 
 
-bool getSymbol(char* data, char* dest)
+bool getSymbol(char* data, char* dest, bool is_label)
 {
-	char *symbols[3] = { ".string", ".data", ".struct" };
 	bool afterLabel = FALSE, detectedSymbol = FALSE;
-	int i, j, length, correctSymbol = -1;
+	int i, j, length, correctSymbol = -1 ;
+	afterLabel = !is_label;
 	length = strlen(data);
 	for (i = 0; i < length; i++)
 	{
@@ -284,9 +317,9 @@ bool getSymbol(char* data, char* dest)
 		{
 			if (!isspace(data[i]))
 			{
-				for (j = 0; j < 3; j++)
+				for (j = 0; j < NUM_OF_DATA_TYPES; j++)
 				{
-					if (strncmp(data + i, symbols[j], strlen(symbols[j])) == 0)
+					if (strncmp(data + i, DataCommand[j], strlen(DataCommand[j])) == 0)
 					{
 						if (detectedSymbol)
 							return FALSE;
@@ -294,7 +327,7 @@ bool getSymbol(char* data, char* dest)
 						{
 							detectedSymbol = TRUE;
 							correctSymbol = j;
-							i += strlen(symbols[j]) - 1;
+							i += strlen(DataCommand[j]) - 1;
 							continue;
 						}
 					}
@@ -324,22 +357,33 @@ bool getSymbol(char* data, char* dest)
 	if (correctSymbol != -1)
 	{
 		if (dest != NULL)
-			strncpy(dest, symbols[correctSymbol], strlen(symbols[correctSymbol]));
+			strncpy(dest, DataCommand[correctSymbol], strlen(DataCommand[correctSymbol]));
 		return TRUE;
 	}
 	return FALSE;
 }
 
-void addNumberToDataList(dataPtr *head, dataPtr *tail, int dc, int num)
+bool addNumberToDataList(dataPtr *head, dataPtr *tail, int dc, int num)
 {
 	if (num >= 0)
 	{
+		if (num > MAX_DATA_NUMBER)
+		{
+			printError(OVERFLOW_DATA_POSITIVE_NUMBER);
+			return FALSE;
+		}
 		addToDataList(head, tail, dc, positiveNumber, num);
 	}
 	else
 	{
+		if (num < MIN_DATA_NUMBER)
+		{
+			printError(OVERFLOW_DATA_NEGATIVE_NUMBER);
+			return FALSE;
+		}
 		addToDataList(head, tail, dc, negativeNumber, num);
 	}
+	return TRUE;
 }
 
 
@@ -357,8 +401,6 @@ AddressingMode getOperandAddressing(char* token)
 	else
 		return -1;
 }
-
-
 
 
 int getCommandSize(char* command)
@@ -534,15 +576,11 @@ int getCommandSize(char* command)
 			return 0;
 		}
 	}
+	printError(INVALID_OPCODE_ERROR);
 	return 0;
 }
 
 
-
-
-/*
-Description: convert 10 word length represents in binary code to "wierd 32 base"
-*/
 void decimalToWierd(int num, char* res)
 {
 	char *a;
@@ -551,9 +589,9 @@ void decimalToWierd(int num, char* res)
 	{
 		num = MAX10BITS - (num*(-1));
 	}
-	a = WIERD_32_BASE[num % 32];
-	num = num / 32;
-	b = WIERD_32_BASE[num % 32];
+	a = WIERD_32_BASE[num % BASE_LENGTH];
+	num = num / BASE_LENGTH;
+	b = WIERD_32_BASE[num % BASE_LENGTH];
 	strcat(res, b);
 	strcat(res, a);
 }
@@ -565,7 +603,6 @@ void binaryToWierd(int *binary, char* res)
 	decimalToWierd(num, res);
 }
 
-/*arr size = 8(number in command) 10(data)*/
 void decimalToBinary(int num, int *binaryNum, int arrSize)
 {
 	int i;
@@ -606,8 +643,7 @@ bool isDataCommand(char* token)
 	{
 		temp++;
 	}
-	printf("%s\n", temp);
-	if (!strncmp(temp, ".data", 5) || !strncmp(temp, ".string", 7) || !strncmp(temp, ".struct", 7))
+	if (!strncmp(temp, DataCommand[0], DataCommandLength[0]) || !strncmp(temp, DataCommand[1], DataCommandLength[1]) || !strncmp(temp, DataCommand[2], DataCommandLength[2]))
 		return TRUE;
 	return FALSE;
 }
@@ -624,7 +660,7 @@ int getNumber(char* token)
 	}
 	num = temp;
 
-	while (*temp >= 0 && *temp <= 255 && !isspace(*temp))
+	while (*temp >= 0 && *temp < MAX8BITS && !isspace(*temp))
 	{
 		end++;
 		temp++;
@@ -695,4 +731,96 @@ bool readLine(FILE* fp, char* line)
 		return FALSE;
 	flag = (fgets(line, MAX_LINE_LENGTH, fp) != NULL) ? TRUE : FALSE;
 	return flag;
+}
+
+void resetLine(char *line, char* labelName, char *data)
+{
+	int i;
+	for (i = 0; i < MAX_LINE_LENGTH + 1; i++)
+	{
+		line[i] = 0;
+		labelName[i] = 0;
+		data[i] = 0;
+	}
+}
+
+
+bool isNumber(char *token)
+{
+	int i = 0;
+	token = trimStr(token);
+	if (token[i] == '+' || token[i] == '-')
+		i++;
+	for (; i < strlen(token); i++)
+	{
+		if (!isdigit(token[i]))
+			return FALSE;
+	}
+	return TRUE;
+}
+
+bool isString(char *token)
+{
+	int i = 0;
+	token = trimStr(token);
+	if (token != NULL && token[i++] == '"' && token[strlen(token) - 1] == '"')
+	{
+		for (; i < strlen(token) - 1; i++)
+		{
+			if (isspace(token[i]))
+				return FALSE;
+		}
+	}
+	else
+	{
+		return FALSE;
+	}
+	return TRUE;
+}
+
+
+bool StartWithCommand(char * line)
+{
+	int i = 0, len = 0;
+	char *token = trimStr(line);
+	char *temp = (char *)calloc(MAX_LINE_LENGTH, sizeof(char));
+	for (i = 0; i < sizeof(temp); i++)
+	{
+		if (isspace(token[i]))
+			break;
+		len++;
+	}
+	strncpy(temp, token, len);
+	for (i = 0; i < NUM_OF_OPCODES; i++)
+	{
+		if (!(strcmp(temp, opcodes[i].opcodeName)))
+		{
+			free(temp);
+			return TRUE;
+		}
+	}
+	free(temp);
+	return FALSE;
+}
+
+bool StartWithDataCommand(char * line)
+{
+	int i = 0;
+	char *token = trimStr(line);
+	char temp[MAX_LINE_LENGTH + 1] = { 0 };
+	for (i = 0; i < sizeof(temp); i++)
+	{
+		if ((*token && (isspace(token[0]))) || *token == '\0')
+			break;
+		temp[i] = token[0];
+		token++;
+	}
+	for (i = 0; i < NUM_OF_DATA_TYPES; i++)
+	{
+		if (!(strcmp(temp, DataCommand[i])))
+		{
+			return TRUE;
+		}
+	}
+	return FALSE;
 }
